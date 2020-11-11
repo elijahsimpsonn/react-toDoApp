54abf18fa19da34e19217d82222e48a8d5aea9b9
Elijah Simpson <elijahsimpson1991@gmail.com>
2020-11-11 09:11:40 -0500

    style/refactor: updated css and prop destructuring

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
-export default function Card(props) {
+export default function Card({ title, content }) {
   return (
-    <div className='Card'>
-      <button
-        type='button'
-      >
-        delete
-      </button>
-      <h3>{props.title}</h3>
-      <p>{props.content}</p>
+    <div className="Card">
+      <button type="button">Delete</button>
+      <h3>{title}</h3>
+      <p>{content}</p>
     </div>
-  )
+  );
 }
--- src/List.js
+++ src/List.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import Card from './Card'
-import './List.css';
+import React from "react";
+import Card from "./Card";
+import "./List.css";
 
-export default function List(props) {
+export default function List({ header, cards }) {
   return (
-    <section className='List'>
-      <header className='List-header'>
-        <h2>{props.header}</h2>
+    <section className="List">
+      <header className="List-header">
+        <h2>{header}</h2>
       </header>
-      <div className='List-cards'>
-        {props.cards.map((card) =>
-          <Card
-            key={card.id}
-            title={card.title}
-            content={card.content}
-          />
-        )}
-        <button
-          type='button'
-          className='List-add-button'
-        >
+      <div className="List-cards">
+        {cards.map((card) => (
+          <Card key={card.id} title={card.title} content={card.content} />
+        ))}
+        <button type="button" className="List-add-button">
           + Add Random Card
         </button>
       </div>
     </section>
-  )
+  );
 }
